a33ff775481bf1ecb929a998c25d52d023598bc1
*prL19ETNMs <rocket.martue@gmail.com>
2021-11-22 09:30:24 +0900

    Update manage_posts_columns.php

--- inc/manage_posts_columns.php
+++ inc/manage_posts_columns.php
@@ -9,7 +9,7 @@ add_filter(
 	'manage_posts_columns',
 	function ( $columns ) {
 		global $post_type;
-		if( in_array( $post_type, array( 'product_content' ) ) ) {
+		if( in_array( $post_type, array( 'product_desc' ) ) ) {
 			$new_columns = array(
 				'shortcode' => esc_html__( 'Shortcode', 'block-editable-cpt-for-woo' ),
 			);
@@ -22,10 +22,13 @@ add_filter(
 add_filter(
 	'manage_posts_custom_column',
 	function ( $column_name, $post_id ) {
-		if ( 'shortcode' === $column_name ) {
-			$post = get_post( $post_id );
-			$slug = $post->post_name;
-			echo '<span class="product_content-short-code">[product_content slug=' . esc_html( $slug ) . ']</span>';
+		global $post_type;
+		if( in_array( $post_type, array( 'product_desc' ) ) ) {
+			if ( 'shortcode' === $column_name ) {
+				$post = get_post( $post_id );
+				$slug = $post->post_name;
+				echo '<span class="product_desc-short-code">[product_desc slug=' . esc_html( $slug ) . ']</span>';
+			}
 		}
 	},
 	10,
